94d899a7bfed3018ac5cf1da0d9c5416ee9054de
fidelisprasetyo <fidelisprasetyo@gmail.com>
2022-11-27 14:09:49 -0800

    random graph

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "floyd_warshall.h"
 #include "dijkstra.h"
 
-#define SANITY_CHECK true
 #define RAND_RANGE 10
 #define MAX_SIZE 20
 #define INF 999999
@@ -24,6 +23,49 @@ void resizeMat(graph& g, int size) {
 	g.resize(size);
 	for(int i = 0; i < size; i++) {
 		g[i].resize(size);
+	}
+}
+
+void randomizeSparseGraph(graph& g, int num_vertex) {
+	int num_edge = num_vertex - 1;	// minimum number of edges possible (sparse)
+
+	std::vector<int> v_list;
+	for(int i = 0; i < num_vertex; i++) {
+		v_list.push_back(i);
+	}
+	std::random_shuffle(v_list.begin(), v_list.end());
+
+	resizeMat(g, num_vertex);
+	for(int i = 0; i < num_vertex; i++) {
+		for(int j = 0; j < num_vertex; j++) {
+			if(i == j) {
+				g[i][j] = 0;
+			}
+			else {
+				g[i][j] = INF;
+			}
+		}
+	}
+
+	for(int i = 1; i < num_vertex; i++) {
+		int prev = v_list[i-1];
+		int cur = v_list[i];
+		g[prev][cur] = getRandInt(1, 10);
+		g[cur][prev] = g[prev][cur];
+	}
+}
+
+void randomizeDenseGraph(graph& g, int num_vertex) {
+	resizeMat(g, num_vertex);
+	for(int i = 0; i < num_vertex; i++) {
+		for(int j = 0; j < num_vertex; j++) {
+			if(i == j) {
+				g[i][j] = 0;
+			}
+			else {
+				g[i][j] = getRandInt(1,10);
+			}
+		}
 	}
 }
 
@@ -53,36 +95,13 @@ int main() {
 	//floydWarshall(g);
 	//dijkstraAllPaths(g1);
 
-	// performance test
-
-	int num_vertex = 10;
-	int num_edges = 9;
-	std::vector<bool> isAdded(num_vertex, false);
-	std::vector<int> v_list;
-
-	for(int i = 0; i < num_vertex; i++) {
-		v_list.push_back(i);
-	}
-
-	std::random_shuffle(v_list.begin(), v_list.end());
-
-	// initialize
-	graph gr(num_vertex, std::vector<int>(num_vertex, INF));
-	for(int i = 0; i < num_vertex; i++) {
-		gr[i][i] = 0;
-	}
-
-	// add vertices
-	for(int i = 1; i < num_vertex; i++) {
-		int prev = v_list[i-1];
-		int cur = v_list[i];
-		gr[prev][cur] = getRandInt(1, 10);
-		gr[cur][prev] = gr[prev][cur];
-	}
-
+	graph gr;
+	randomizeSparseGraph(gr, 4);
+	printMatrix(gr);
+	randomizeDenseGraph(gr, 4);
 	printMatrix(gr);
 
-	floydWarshall(gr);
+	//floydWarshall(gr);
 
     return 0;
     
